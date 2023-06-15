f28eadb097d88e8085eb9d1b104636bf599500e6
Eduardo Zafra <duduzafra@gmail.com>
2023-06-15 20:14:27 +0200

    get_next_line

---
+++ 42-cursus/get_next_line/get_next_line.c
@@ -0,0 +1,103 @@
+#include "get_next_line.h"
+
+static char	*ft_next_line(char *buffer)
+{
+	int		i;
+	int		j;
+	char	*leftover;
+
+	i = 0;
+	while (buffer[i] && buffer[i] != '\n')
+		i++;
+	if (!buffer[i])
+	{
+		free(buffer);
+		return (NULL);
+	}
+	leftover = ft_calloc(ft_strlen(buffer) - i + 1, sizeof(char));
+	i++;
+	j = 0;
+	while (buffer[i])
+		leftover[j++] = buffer[i++];
+	leftover[j] = '\0';
+	free(buffer);
+	return (leftover);
+}
+
+static char	*ft_line(char *buffer)
+{
+	int		i;
+	char	*line;
+
+	i = 0;
+	if (!buffer[i])
+		return (NULL);
+	while (buffer[i] && buffer[i] != '\n')
+		i++;
+	line = ft_calloc(i + 2, sizeof(char));
+	i = 0;
+	while (buffer[i] && buffer[i] != '\n')
+	{
+		line[i] = buffer[i];
+		i++;
+	}
+	if (buffer[i] == '\n')
+		line[i] = '\n';
+	return (line);
+}
+
+static char	*ft_read_line(int fd, char *buffer)
+{
+	char	*tmp;
+	ssize_t	read_bytes;
+
+	tmp = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+	read_bytes = 1;
+	while (read_bytes != 0 && !ft_strchr(buffer, '\n'))
+	{
+		read_bytes = read(fd, tmp, BUFFER_SIZE);
+		if (read_bytes == -1)
+		{
+			free(buffer);
+			free(tmp);
+			return (NULL);
+		}
+		tmp[read_bytes] = '\0';
+		buffer = ft_strjoin(buffer, tmp);
+	}
+	free(tmp);
+	return (buffer);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*buffer;
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	buffer = ft_read_line(fd, buffer);
+	if (!buffer)
+		return (NULL);
+	line = ft_line(buffer);
+	buffer = ft_next_line(buffer);
+	return (line);
+}
+
+/*int	main(void)
+{
+	int	fd;
+
+	fd = open("prueba.txt", O_RDONLY);
+	if (fd == -1)
+	{
+		perror ("open");
+		exit(EXIT_FAILURE);
+	}
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	close(fd);
+	return (0);
+}
+*/
---
+++ 42-cursus/get_next_line/get_next_line.h
@@ -0,0 +1,19 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+char	*get_next_line(int fd);
+int		ft_strlen(char *s);
+char	*ft_strchr(char *s, int c);
+void	*ft_calloc(size_t count, size_t size);
+char	*ft_strjoin(char *s1, char *s2);
+
+#endif
