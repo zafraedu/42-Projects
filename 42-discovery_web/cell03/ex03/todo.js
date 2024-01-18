aaa83c04d9f7b7d8331d9f046041b5e111545e53
Eduardo Zafra <duduzafra@gmail.com>
2024-01-18 08:48:25 +0100

    PDW c3e3

--- 42-discovery_web/cell03/ex03/todo.js
+++ 42-discovery_web/cell03/ex03/todo.js
@@ -1,1 +1,28 @@
-"use strict";
+const btnAdd = document.querySelector("#btn-add");
+const ToDo = document.querySelector("#ToDo");
+let itemArray = [];
+
+btnAdd.addEventListener("click", () => {
+  const itemList = window.prompt("list");
+  if (itemList === null || itemList.trim() === "") return;
+
+  itemArray.unshift(itemList);
+  actualizarLista();
+});
+
+function actualizarLista() {
+  ToDo.innerHTML = "";
+
+  for (let i of itemArray) {
+    let itemCreate = document.createElement("div");
+    itemCreate.setAttribute("class", "item");
+    itemCreate.innerHTML = `${i}`;
+
+    itemCreate.addEventListener("click", () => {
+      if (window.confirm("c") === false) return;
+      itemArray = itemArray.filter((e) => e !== i);
+      actualizarLista();
+    });
+    ToDo.appendChild(itemCreate);
+  }
+}
