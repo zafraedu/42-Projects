43495b9f4dc58f7657d6c000b1049949a9259016
Eduardo Zafra <duduzafra@gmail.com>
2024-04-08 15:10:44 +0200

    finish m4 e0

--- 42-cursus/circle_4/CPP_Module_0-4/Module_04/ex00/main.cpp
+++ 42-cursus/circle_4/CPP_Module_0-4/Module_04/ex00/main.cpp
@@ -1,16 +1,36 @@
-#include "inc/Animal.hpp"
-#include "inc/Dog.hpp"
-#include "inc/Cat.hpp"
+#include "Animal.hpp"
+#include "Dog.hpp"
+#include "Cat.hpp"
+#include "WrongAnimal.hpp"
+#include "WrongCat.hpp"
 
 int main()
 {
-	const Animal *meta = new Animal();
-	const Animal *j = new Dog();
-	const Animal *i = new Cat();
-	std::cout << j->getType() << " " << std::endl;
-	std::cout << i->getType() << " " << std::endl;
-	i->makeSound(); // will output the cat sound!
-	j->makeSound();
-	meta->makeSound();
-	return 0;
+const Animal* meta = new Animal();
+// the pointer itself is const not the object it points to
+const Animal* j = new Dog();
+const Animal* i = new Cat();
+std::cout << j->getType() << " " << std::endl;
+std::cout << i->getType() << " " << std::endl;
+i->makeSound(); //will output the cat sound!
+j->makeSound();
+meta->makeSound();
+
+
+std::cout << "-------------------" << std::endl;
+ WrongAnimal* wrong = new WrongAnimal();
+    wrong->makeSound();
+    wrong->getType();
+    delete wrong;
+    wrong = new WrongCat();
+    wrong->makeSound();
+    wrong->getType();
+    delete wrong;
+    std::cout << "-------------------" << std::endl;
+
+delete meta;
+delete i;
+delete j;
+//
+return 0;
 }
